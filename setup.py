fb6d55dfa9ec6cfac3333b8ff9bf4fa179fc36e2
Hector Acosta <hector.acosta@gmail.com>
2012-03-15 00:50:19 -0600

    Added setup.py
    Signed-off-by: Hector Acosta <hector.acosta@gmail.com>

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='python-vim',
+		version='0.1',
+		description='Python utility that integrates the python interactive console with vim',
+		author='Héctor Acosta',
+		author_email='hector.acosta@gmail.com',
+		py_modules=['vim'],
+		)
