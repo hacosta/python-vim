43e83d81dde4edb1dbfd4a5867b0be2710b87550
Hector Acosta <hector.acosta@gmail.com>
2014-07-18 12:17:49 -0500

    Make this module pep8 firendly
    Signed-off-by: Hector Acosta <hector.acosta@gmail.com>

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@
 from distutils.core import setup
 
 setup(name='python-vim',
-		version='0.1',
-		description='Python utility that integrates the python interactive console with vim',
-		author='Héctor Acosta',
-		author_email='hector.acosta@gmail.com',
-		py_modules=['vim'],
-		)
+      version='0.1',
+      description='Python utility that integrates the python interactive console with vim',
+      author='Héctor Acosta',
+      author_email='hector.acosta@gmail.com',
+      py_modules=['vim'],
+      )
--- vim.py
+++ vim.py
@@ -1,26 +1,28 @@
 import subprocess
 import tempfile
 import os
+import os.path
 import atexit
 
 temp_filename = tempfile.mkstemp('.py')[1]
+mtime = os.path.getmtime(temp_filename)
 atexit.register(os.remove, temp_filename)
 
-def vim(editor=''):
-	""" Vim function, takes an alternative editor as a param. Will launch an editor instance and exec the code written to that file"""
-	editor = editor or os.getenv('EDITOR') or 'vi'
-	before = open(temp_filename).read()
-	subprocess.call([editor, temp_filename])
-	after = open(temp_filename).read()
-	if before != after:
-		new_locals = {}
-		try:
-			exec(after, globals(), new_locals)
-		except SystemExit:
-			pass
-		for k, v in new_locals.items():
-			try: #nasty hack to include the vars back on the interpreter
-				__builtin__[k] = v 
-			except:
-				__builtins__[k] = v
 
+def vim(editor=''):
+    """ Vim function, takes an alternative editor as a param. Will launch an editor instance and exec the code written to that file"""
+    editor = editor or os.getenv('EDITOR') or 'vi'
+    before = open(temp_filename).read()
+    subprocess.call([editor, temp_filename])
+    after = open(temp_filename).read()
+    if before != after:
+        new_locals = {}
+        try:
+            exec(after, globals(), new_locals)
+        except SystemExit:
+            pass
+        for k, v in new_locals.items():
+            try:  # hack to include the vars back on the interpreter
+                __builtin__[k] = v
+            except:
+                __builtins__[k] = v
