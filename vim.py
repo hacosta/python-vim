47606eb665db80fb5156f72dfdf407270f917cb5
Hector Acosta <hector.acosta@gmail.com>
2014-07-18 12:25:32 -0500

    Execute on mtime modification rather than file changes
    Signed-off-by: Hector Acosta <hector.acosta@gmail.com>

--- vim.py
+++ vim.py
@@ -5,20 +5,22 @@ import os.path
 import atexit
 
 temp_filename = tempfile.mkstemp('.py')[1]
-mtime = os.path.getmtime(temp_filename)
 atexit.register(os.remove, temp_filename)
 
 
 def vim(editor=''):
     """ Vim function, takes an alternative editor as a param. Will launch an editor instance and exec the code written to that file"""
+
+    last_mtime = os.path.getmtime(temp_filename)
     editor = editor or os.getenv('EDITOR') or 'vi'
-    before = open(temp_filename).read()
     subprocess.call([editor, temp_filename])
-    after = open(temp_filename).read()
-    if before != after:
+
+    if os.path.getmtime(temp_filename) != last_mtime:
+        # File has been saved, update env
+        last_mtime = os.path.getmtime(temp_filename)
         new_locals = {}
         try:
-            exec(after, globals(), new_locals)
+            exec(open(temp_filename).read(), globals(), new_locals)
         except SystemExit:
             pass
         for k, v in new_locals.items():
