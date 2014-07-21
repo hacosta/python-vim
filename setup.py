0eed0da3b7e31cc3c29d0d386fa7a2b5d2dbb73a
Hector Acosta <hector.acosta@gmail.com>
2014-07-21 08:50:19 -0500

    Add license
    Signed-off-by: Hector Acosta <hector.acosta@gmail.com>

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@
 from distutils.core import setup
 
 setup(name='python-vim',
-      version='0.2',
+      version='0.3',
       description='Python utility that integrates the python interactive console with vim',
       author='Héctor Acosta',
       author_email='hector.acosta@gmail.com',
       url='https://github.com/hacosta/python-vim',
       py_modules=['vim'],
+      license='BSD'
       )
